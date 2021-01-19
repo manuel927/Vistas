84d5fb4295ed1f146f148dcec9fd734988c2bb1f
manuel927 <manuel.smo24@gmail.com>
2021-01-19 17:28:59 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/unipiloto/vistas/MainActivity.java
@@ -0,0 +1,22 @@
+package co.edu.unipiloto.vistas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        //setContentView(R.layout.ejemplo2);
+        //setContentView(R.layout.ejemplo3);
+        //setContentView(R.layout.ejemplo4);
+        //setContentView(R.layout.ejemplo5);
+        //setContentView(R.layout.ejemplo6);
+        //setContentView(R.layout.ejemplo7);
+        //setContentView(R.layout.ejemplo8);
+        //setContentView(R.layout.ejemplo9);
+        setContentView(R.layout.ejemplo10);
+    }
+}
